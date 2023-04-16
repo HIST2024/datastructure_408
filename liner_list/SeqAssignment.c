0517087c4523a5034456ab44d01c5d5d60e44762
codermao <1301878596@qq.com>
2023-04-16 18:18:53 +0800

    创建

---
+++ liner_list/SeqAssignment.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "./seqlist01.c"
+
+//删除最小元素，并返回删除的值,空出的位置由最后一个元素来填补，若顺序表为空，显示出错信息
+void question01(SeqList *L, Elemtype *e) {
+  if (L->length == 0) {
+    printf("error! list is empty!");
+    return;
+  }
+  Elemtype minElem = L->data[0];
+  int pos = 0;
+  for (int i = 1; i < L->length; i++) {
+    if (L->data[i] < minElem) {
+      pos = i;
+      minElem = L->data[i];
+    }
+  }
+  *e = minElem;
+  L->data[pos] = L->data[L->length - 1];
+  L->length--;
+}
+
+//设计一个高效算法，将顺序表中元素逆置，要求空间复杂度O(1)
+void question02(SeqList *L) {
+  Elemtype temp = 0;
+  for (int i = 0; i < L->length / 2;i++) {
+    temp = L->data[i];
+    L->data[i] = L->data[L->length - 1 - i];
+    L->data[L->length - 1 - i] = temp;
+  }
+}
+
+//删除顺序表中所有值为x的元素，时间复杂度O(n)、空间复杂度O(1)
+void question03_01(SeqList *L, Elemtype x) {
+  int k = 0;//记录值不等于x的元素个数
+  for (int i = 0; i < L->length; i++) {
+    if (L->data[i] != x) {
+      L->data[k] = L->data[i];
+      k++;//不等于x的元素增1
+    }
+  }
+  L->length = k;//顺序表的长度等于k
+}
+void question03_02(SeqList *L, Elemtype x) {
+  int k = 0, i = 0; //k值记录等于x的元素个数
+  while (i < L->length) {
+    if (L->data[i] == x)
+      k++;
+    else
+      L->data[i - k] = L->data[i];//当前元素前移k个位置
+    i++;
+  }
+  L->length = L->length - k;//顺序表L的长度递减
+}
+
+// 
+void question04(SeqList *L, Elemtype x) {
+  
+}
+
+void main() {
+  SeqList L;
+  initList(&L);
+  listInsert(&L, 1, 3);
+  listInsert(&L, 2, 5);
+  listInsert(&L, 3, 3);
+  listInsert(&L, 4, 9);
+  listInsert(&L, 5, 11);
+  listInsert(&L, 6, 3);
+  listInsert(&L, 7, 13);
+  printList(&L);
+
+  // Elemtype e;
+  // question01(&L, &e);
+  // printList(&L);
+  // printf("删除的最小元素是:%d", e);
+
+  question03_01(&L, 3);
+  printList(&L);
+}
---
+++ liner_list/Seqlist02.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define InitSize 10
+typedef int Elemtype;
+// 动态分配
+typedef struct {
+  Elemtype *data;
+  int MaxSize;
+  int length;
+} SeqList;
+
+// 初始化顺序表
+void initList(SeqList *L) {
+  L->data = (Elemtype *)malloc(InitSize * sizeof(Elemtype));
+  L->length = 0;
+  L->MaxSize = InitSize;
+}
+
+// 增加动态数组的长度
+void increaseSize(SeqList *L, int len) {
+  Elemtype *p = L->data;
+  L->data = (Elemtype *)malloc((L->MaxSize + len) * sizeof(Elemtype));
+  for (int i = 0; i < L->length; i++) {
+    L->data[i] = p[i];
+  }
+  L->MaxSize += len;
+  free(p);
+}
+
+//插入元素
+void listInsert(SeqList *L, int i, Elemtype e) {
+  if (i < 1 || i > L->length + 1) {
+    printf("ListInsert error1");
+    return;
+  }
+  if (L->length >= L->MaxSize) {
+    printf("ListInsert error2");
+    return;
+  }
+  for (int j = L->length; j >= i;j--) {
+    L->data[j] = L->data[j - 1];
+  }
+  L->data[i - 1] = e;
+  L->length++;//顺序表长度加一
+}
+
+//删除元素
+void listDelete(SeqList *L,int i,Elemtype *e) {
+  if (i < 1 || i >L->length) {
+    printf("ListDelete error1!");
+    return;
+  }
+  *e = L->data[i - 1];
+  for (int j = i; j < L->length; j++) {
+    L->data[j - 1] = L->data[j];
+  }
+  L->length--;
+}
+
+//按位查找
+Elemtype getElem(SeqList L, int i) {
+  if (i < 1 || i >L.length) {
+    printf("listDelete error1!");
+    return -1;
+  }
+  return L.data[i - 1];
+}
+
+//按值查找
+int locateElem(SeqList *L, Elemtype e) {
+  for (int i = 0; i < L->length; i++) {
+    if (e == L->data[i]) {
+      return i + 1;//返回该值的位序
+    }
+  }
+  return -1;//返回-1表示顺序表中没有该值
+}
+
+//打印顺序表
+void printList(SeqList *L) {
+  for (int i = 0; i < L->length; i++) {
+    printf("%d\t", L->data[i]);
+  }
+  printf("\n");
+}
+
+void main01() {
+  SeqList L;
+  initList(&L);
+  listInsert(&L, 1, 3);
+  listInsert(&L, 2, 5);
+  listInsert(&L, 3, 7);
+  printList(&L);
+  
+  Elemtype e;
+  listDelete(&L, 2, &e);
+  printList(&L);
+  printf("delete element is = %d", e);
+
+  printf("第二个元素是:%d\n", getElem(L, 2));
+
+  if (locateElem(&L, 7) != -1) {
+    printf("7的位置是:%d", locateElem(&L, 7));
+  } else {
+    printf("顺序表中没有7");
+  }
+}
